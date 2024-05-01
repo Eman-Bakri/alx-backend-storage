76456d550c77fcb9e1a2266bba47ff80e56e40af
Eman-Bakri <eman1996-sy@hotmail.com>
2024-04-30 17:37:53 -0700

    NoSQL MongoDB

--- 0x01-NoSQL/9-main.py
+++ 0x01-NoSQL/9-main.py
@@ -2,7 +2,7 @@
 """ 9-main """
 from pymongo import MongoClient
 list_all = __import__('8-all').list_all
-insert_schoool = __import__('9-insert_school').insert_school
+insert_school = __import__('9-insert_school').insert_school
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
@@ -13,3 +13,4 @@ if __name__ == "__main__":
     schools = list_all(school_collection)
     for school in schools:
         print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('address', "")))
+
