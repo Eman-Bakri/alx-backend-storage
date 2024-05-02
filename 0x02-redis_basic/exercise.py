5b946a46776db4bac573667222cef0eff97035bc
Eman-Bakri <eman1996-sy@hotmail.com>
2024-05-02 00:38:39 +0000

    Redis Basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -52,7 +52,7 @@ def call_history(method: Callable) -> Callable:
 def replay(func: Callable):
     """
     Prototype: def replay(func: Callable):
-    Displays history of calls of a particular function
+    Returns: Displays history of calls of a particular function
     """
     r = redis.Redis()
     key_m = func.__qualname__
